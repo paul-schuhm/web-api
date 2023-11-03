0aed3a4d62e438e9fa9ca9f50fa7ac14faaa79c3
p. schuhmacher <contact@pschuhmacher.com>
2023-11-03 08:54:42 +0100

    met a jour la démo mysql2 avec requetes sql tests sans et avec api des promesses

--- demos/demo-node-express-mysql2/README.md
+++ demos/demo-node-express-mysql2/README.md
@@ -5,6 +5,7 @@ Une simple démo d'application Node.js/Express.js connectée à une base de donn
 - [Démo application Node.js/Express.js avec connexion à une base de données relationnelle avec mysql2](#démo-application-nodejsexpressjs-avec-connexion-à-une-base-de-données-relationnelle-avec-mysql2)
   - [Pré-requis](#pré-requis)
   - [Lancer le projet](#lancer-le-projet)
+  - [Tester](#tester)
   - [Références utiles](#références-utiles)
 
 
@@ -35,14 +36,20 @@ node --watch ./bin/www
 npm start
 ~~~
 
-Tester
+## Tester
+
+Tester le module `mysql2` avec et sans l'API des promesses. Dans le second cas, on retombe sur le module originel `mysql` où il faut enregistrer des callbacks au retour du résultat de la requête sql, ce qui amène au *callback hell*.
 
 ~~~bash
-curl localhost:3000/test
+curl localhost:3000/test-without-promises-api
+curl localhost:3000/test-with-promises-api
+curl localhost:3000/test-prepared-statement
+curl localhost:3000/test-chaining-sql-requests
 ~~~
 
 ## Références utiles
 
 - [Le starter pack API](https://github.com/paul-schuhm/starterpack-api-nodejs) (Node.js/Express.js, MySQL et Adminer)
 - [mysql2](https://www.npmjs.com/package/mysql2), le module client mysql qui implémente l'API des promesses
+- [mysql](https://www.npmjs.com/package/mysql), le module client mysql originel
 - [Adminer](https://www.adminer.org/), le site officiel d'Adminer, un outil de gestion de base de données graphique qui tient dans un seul fichier PHP
--- demos/demo-node-express-mysql2/routes/index.js
+++ demos/demo-node-express-mysql2/routes/index.js
@@ -1,28 +1,82 @@
 var express = require('express');
 var router = express.Router();
-const mysql  = require('mysql2')
+//Module mysql2 sans l'api des promesses (avec callback)
+const mysqlCallback = require('mysql2');
+//Module mysql2 avec l'api des promesses
+const mysql = require('mysql2/promise');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-//Ouvrir une connexion
-const connection = mysql.createConnection({
+/**
+ * Informations de connexion à la base (DSN)
+ */
+const dsn = {
   host: '127.0.0.1',
   database: 'mydb',
   user: 'user',
   password: 'password',
   port: 5002
-})
-connection.connect()
+};
 
-router.get('/test', function (req, res, next) {
-  //Tester
-  connection.query('SELECT * FROM User', (err, rows, fields) => {
-    if (err) throw err
-    res.send(rows);
-  })
+// Une connexion à la base de données qui n'utilise pas l'API des promesses
+const connectionUsingCallBack = mysqlCallback.createConnection(dsn);
+
+/**
+ * Un simple test sans l'API des promesses
+ */
+router.get('/test-without-promises-api', async function (req, res, next) {
+  //Requête avec une callback
+  connectionUsingCallBack.execute(
+    'SELECT * FROM `User`',
+    function (err, results, fields) {
+      res.send(results)
+    }
+  );
+})
+
+/**
+ * Un simple test avec l'API des promesses de mysql2
+ */
+router.get('/test-with-promises-api', async function (req, res, next) {
+  //Requête avec une promesse
+  const connection = await mysql.createConnection(dsn);
+  const [result] = await connection.execute('SELECT * FROM User')
+  res.send(result)
+})
+
+/**
+ * Test des requêtes préparées
+ */
+router.get('/test-prepared-statement', function (req, res, next) {
+  //Requête préparée avec une callback
+  connectionUsingCallBack.execute(
+    //Données remplacées par un placeholder '?', puis données ensuite dans un tableau (même ordre)
+    'SELECT * FROM `User` WHERE `first_name` = ? OR `first_name` = ?',
+    ['John', 'Eve; DROP TABLE users'], //tentative d'injection SQL écartée
+    function (err, results, fields) {
+      console.log(results); // results contains rows returned by server
+      console.log(fields); // fields contains extra meta data about results, if available
+      // If you execute same statement again, it will be picked from a LRU cache
+      // which will save query preparation time and give better performance
+      res.send(results)
+    }
+  );
+})
+
+/**
+ * Enchaîner des requêtes (attendre le resultat de la précédente pour effectuer la suivante sans bloquer l'application)
+ * avec l'API des promesses (éviter le callback hell de mysqlCallBack)
+ */
+router.get('/test-chaining-sql-requests', async function (req, res, next) {
+  //Requêtes préparées et enchainées avec les promesses (sans callback hell)
+  const connection = await mysql.createConnection(dsn);
+  const [rows1, fields1] = await connection.execute('SELECT * FROM `User` WHERE `first_name` = ?', ['John']);
+  const [rows2, fields2] = await connection.execute('SELECT * FROM `User` WHERE `first_name` = ?', ['Eve']);
+  console.log(rows1)
+  res.send([rows1, rows2])
 })
 
 module.exports = router;
