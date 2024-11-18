6e1c2798ce7cba99b8b5ef6999bb12bcd9a1ec5d
p schuhmacher <contact@pschuhmacher.com>
2024-11-18 14:01:58 +0100

    ajoute script generation clef privée

---
+++ demos/api-billeterie-live/genkey.js
@@ -0,0 +1,10 @@
+//Script pour générer une nouvelle clef secrete dans le fichier "private.key"
+const crypto = require('crypto');
+const fs = require('fs');
+
+//Génère une clef aléatoire de 32 bytes (char) en ascii
+const secret = crypto.randomBytes(32).toString('hex');
+//Enregistrer clef dans un fichier private.key
+
+fs.writeFileSync('private.key', secret);
+
